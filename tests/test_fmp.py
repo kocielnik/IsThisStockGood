14c26dc793d014fb56098498ab49725012be384a
Patryk Kocielnik <patryk@kocielnik.pl>
2024-01-27 07:36:08 +0100

    Update.

--- tests/test_fmp.py
+++ tests/test_fmp.py
@@ -1,6 +1,17 @@
 import keyring as kr
 from os import environ
+import requests
 
+
+def get_statement(symbol):
+  email = environ['DEV_EMAIL']
+  api_key = get_api_key(email)
+
+  return requests.get(
+    "https://financialmodelingprep.com/api/v3/balance-sheet-statement"
+    f"/{symbol}"
+    f"?period=annual&apikey={api_key}"
+  ).json()[0]
 
 def get_api_key(email):
   return kr.get_password("financialmodelingprep.com", email)
@@ -11,3 +22,8 @@ def test_get_email():
 def test_get_password():
   password = get_api_key(environ['DEV_EMAIL'])
   assert isinstance(password, str)
+
+def test_get_fmp_data():
+  data = get_statement("AAPL")
+
+  assert isinstance(data['longTermDebt'], int)
