3195b26cabc4d6fce50cd8aad820240563710d12
Patryk Kocielnik <patryk@kocielnik.pl>
2024-04-07 21:21:33 +0200

    Mock out all interfaces in FMP tests.

--- test/test_fmp.py
+++ test/test_fmp.py
@@ -1,63 +1,88 @@
-import keyring as kr
+"""
+Data source:
+
+Financial Modeling Prep, financialmodelingprep.com
+"""
+
 from os import environ
+from unittest.mock import MagicMock
+import keyring as kr
 import requests
 from requests_toolbelt.sessions import BaseUrlSession
 
 
-def get_api_key(email=environ['DEV_EMAIL']):
-  return kr.get_password("financialmodelingprep.com", email)
+class FinancialModelingPrep:
+    def __init__(self, roic):
+        self.roic = roic
+
+class MockResponse:
+    def __init__(self, get_value):
+        self.get_value = get_value
+
+    def json(self):
+        return self.get_value
+
+
+class MockRequests:
+    def __init__(self, get_value=None):
+        self.get = MagicMock(return_value=get_value)
+
+
+def get_api_key(email=environ["DEV_EMAIL"]):
+    return kr.get_password("financialmodelingprep.com", email)
+
 
 def get_session():
-  return BaseUrlSession(
-    base_url="https://financialmodelingprep.com/api/v3/"
-  )
-
-def get_statement(symbol, api_key=get_api_key()):
-  return requests.get(
-    "https://financialmodelingprep.com/api/v3/balance-sheet-statement"
-    f"/{symbol}"
-    f"?period=annual&apikey={api_key}"
-  ).json()[0]
-
-def get_key_metrics(symbol, api_key=get_api_key()):
-  return requests.get(
-    "https://financialmodelingprep.com/api/v3/key-metrics"
-    f"/{symbol}"
-    f"?apikey={api_key}"
-  ).json()[0]
-
-def get_key_metrics_session(
-    symbol, session=get_session(), api_key=get_api_key()
-  ):
-  return session.get(f"key-metrics/{symbol}?apikey={api_key}").json()[0]
+    return BaseUrlSession(base_url="https://financialmodelingprep.com/api/v3/")
 
 
-class FinancialModelingPrep:
-  def __init__(self, roic):
-    self.roic = roic
+def get_statement(symbol, api_key=get_api_key(), session=requests):
+    return session.get(
+        "https://financialmodelingprep.com/api/v3/balance-sheet-statement"
+        f"/{symbol}"
+        f"?period=annual&apikey={api_key}"
+    ).json()[0]
+
+
+def get_key_metrics(symbol, api_key=get_api_key(), session=requests):
+    return session.get(
+        "https://financialmodelingprep.com/api/v3/key-metrics"
+        f"/{symbol}"
+        f"?apikey={api_key}"
+    ).json()[0]
+
+
+def get_key_metrics_session(symbol, session=get_session(), api_key=get_api_key()):
+    return session.get(f"key-metrics/{symbol}?apikey={api_key}").json()[0]
+
 
 
 def test_get_email():
-  assert 'DEV_EMAIL' in environ
+    assert "DEV_EMAIL" in environ
+
 
 def test_get_password():
-  password = get_api_key()
-  assert isinstance(password, str)
+    password = get_api_key()
+    assert isinstance(password, str)
+
 
 def test_get_fmp_data():
-  data = get_statement("AAPL")
+    data = get_statement("AAPL", session=MockRequests(get_value=MockResponse([{'longTermDebt': 1}])))
 
-  assert isinstance(data['longTermDebt'], int)
+    assert isinstance(data["longTermDebt"], int)
+
 
 def test_get_roic():
-  key_metrics = get_key_metrics("AAPL")
+    key_metrics = get_key_metrics("AAPL", session=MockRequests(get_value=MockResponse([{'roic': 1.0}])))
 
-  assert isinstance(key_metrics['roic'], float)
+    assert isinstance(key_metrics["roic"], float)
+
 
 def test_session():
-  metrics = get_key_metrics_session("AAPL")
-  assert isinstance(metrics['roic'], float)
+    metrics = get_key_metrics_session("AAPL", session=MockRequests(get_value=MockResponse([{'roic': 1.0}])))
+    assert isinstance(metrics["roic"], float)
+
 
 def test_fmp():
-  fmp = FinancialModelingPrep(roic=0.1)
-  assert fmp.roic == 0.1
+    fmp = FinancialModelingPrep(roic=0.1)
+    assert fmp.roic == 0.1
