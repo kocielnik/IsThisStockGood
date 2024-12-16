8403cf3c3e8094f4afe0818214dd409189127f97
Patryk Kocielnik <patryk@kocielnik.pl>
2024-12-16 21:36:08 +0100

    Skip debug mode.

--- main.py
+++ main.py
@@ -2,4 +2,3 @@ from isthisstockgood.DataFetcher import fetchDataForTickerSymbol
 from isthisstockgood.server import create_app
 
 app = create_app(fetchDataForTickerSymbol)
-app.run(host='127.0.0.1', port=8080, debug=True)
