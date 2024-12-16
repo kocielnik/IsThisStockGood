b12a59516056be6445982e9c50870d9f6907bb8d
Patryk Kocielnik <patryk@kocielnik.pl>
2024-12-16 21:32:43 +0100

    Create variable `app` unconditionally.
    Error was:
    
    ```
    Missing variable `handler` or `app` in file "main.py".
    ```

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 from isthisstockgood.DataFetcher import fetchDataForTickerSymbol
 from isthisstockgood.server import create_app
 
-
-if __name__ == '__main__':
-  app = create_app(fetchDataForTickerSymbol)
-  app.run(host='127.0.0.1', port=8080, debug=True)
+app = create_app(fetchDataForTickerSymbol)
+app.run(host='127.0.0.1', port=8080, debug=True)
